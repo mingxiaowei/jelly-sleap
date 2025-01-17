2d02a96f24d0d1e6e13b850f63e15774b431fad0
mingxiaowei <mingxiaowei@berkeley.edu>
2025-01-17 13:22:47 -0800

    add inference scripts for centroid model

---
+++ sleap/jelly/centroid_model_inference.sh
@@ -0,0 +1,9 @@
+#bin/bash
+cd /home/mingxiao/Desktop/
+MODEL_FOLDER="/home/mingxiao/jellyfish/label/multifish/models/250117_104940.centroid.n=1653"
+OUTPUT_PATH="centroid_output_1.slp"
+# VIDEO_PATH="/home/mingxiao/jellyfish/video/full_video_1.avi"
+VIDEO_PATH="/home/mingxiao/jellyfish/label/multifish/multifish_animal_1_v3.slp"
+
+sleap-track -m $MODEL_FOLDER -o $OUTPUT_PATH $VIDEO_PATH --open-in-gui  --only-labeled-frames
+# --only-suggested-frames
