94cc922ec56b2a14b29c701f006468b0bf32e986
mingxiaowei <mingxiaowei@berkeley.edu>
2025-01-17 18:12:33 -0800

    more scripts

--- sleap/jelly/original_model_inference_2.sh
+++ sleap/jelly/original_model_inference_2.sh
@@ -1,9 +1,9 @@
 #bin/bash
 cd /home/mingxiao/Desktop/
-MODEL_FOLDER="/home/mingxiao/jellyfish/label/multifish/models/250117_104940.centroid.n=1653"
-OUTPUT_PATH="centroid_output_1.slp"
+MODEL_FOLDER="/home/mingxiao/jellyfish/label/models/250117_103127.single_instance.n=1653"
+OUTPUT_PATH="original_output_a2.slp"
 # VIDEO_PATH="/home/mingxiao/jellyfish/video/full_video_1.avi"
-VIDEO_PATH="/home/mingxiao/jellyfish/label/multifish/multifish_animal_1_v3.slp"
+VIDEO_PATH="/home/mingxiao/jellyfish/label/animal_2_labels.v001.slp"
 
 sleap-track -m $MODEL_FOLDER -o $OUTPUT_PATH $VIDEO_PATH --open-in-gui  
 # --only-labeled-frames
