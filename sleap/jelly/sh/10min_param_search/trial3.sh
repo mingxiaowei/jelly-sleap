b21ac4b1de2fb945630ad953360844caf3e12f1b
mingxiaowei <mingxiaowei@berkeley.edu>
2025-01-29 14:59:54 -0800

    flowmax track 10min video predicted with latest model

---
+++ sleap/jelly/sh/10min_param_search/trial3.sh
@@ -0,0 +1,35 @@
+#bin/bash
+cd /home/mingxiao/Desktop/jellyfish/
+PREDICTION_FOLDER="video/video_1_clips/predictions/"
+OUTPUT_FOLDER="video/video_1_clips/predictions/flowmax-tracking/"
+
+if [ ! -d "$OUTPUT_FOLDER" ]; then
+    mkdir -p "$OUTPUT_FOLDER"
+fi
+
+n_threads=40
+export OMP_NUM_THREADS=$n_threads
+export OPENBLAS_NUM_THREADS=$n_threads
+export MKL_NUM_THREADS=$n_threads
+export NUMEXPR_NUM_THREADS=$n_threads
+export VECLIB_MAXIMUM_THREADS=$n_threads
+
+PREDICTION_PATH="/home/mingxiao/Desktop/jellyfish/video/video_1_clips/predictions/flowmax-tracking/reencoded_tracking_10min_flowmax.slp"
+OUTPUT_PATH="${OUTPUT_FOLDER}reencoded_10min_t3.slp"
+
+# enable prediction score weighing + use tracking window of size 8
+sleap-track $PREDICTION_PATH \
+    -o $OUTPUT_PATH \
+    --video.index 0 \
+    --batch_size 500 \
+    --tracking.max_tracking 1 \
+    --tracking.tracker flowmaxtracks \
+    --tracking.max_tracks 17 \
+    --tracking.similarity centroid \
+    --tracking.match hungarian \
+    --tracking.track_window 10 \
+    --tracking.oks_score_weighting 1 \
+    --tracking.post_connect_single_breaks 1 \
+    --tracking.robust 0.9 \
+    --verbosity rich \
+    --no-empty-frames
