0291d34a7e048b75623f1955a93186a7e6a69dc4
mingxiaowei <mingxiaowei@berkeley.edu>
2025-02-05 15:52:51 -0800

    use a 5min dataset instead

--- sleap/jelly/autoencoder/model.py
+++ sleap/jelly/autoencoder/model.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras import layers
 
-def build_model():
+def multihead_attention_model():
     # Inputs
     video_input = layers.Input(shape=(170, 174, 1), name="video_input")
     points_input = layers.Input(shape=(17, 2), name="points_input")
@@ -29,3 +30,36 @@ def build_model():
         outputs=corrected_points
     )
     return model
+    
+def feedforward_model_pts_only():
+    points_input = layers.Input(shape=(17, 2), name="points_input")
+    
+    # Encoder
+    x = layers.Flatten()(points_input)
+    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dense(32, activation='relu')(x)
+    encoded = layers.Dense(16, activation='relu', name='encoded')(x)
+    
+    # Decoder
+    x = layers.Dense(32, activation='relu')(encoded)
+    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dense(34, activation='relu')(x)  # 17 points * 2 coordinates
+    decoded = layers.Reshape((17, 2), name='decoded')(x)
+    
+    model = tf.keras.Model(
+        inputs=points_input,
+        outputs=decoded,
+        name='points_autoencoder'
+    )
+    return model
+
+non_missing_frame_indices_path = '/home/mingxiao/Desktop/jellyfish/video/video_1_clips/non_missing_frame_indices.npy'
+
+def labeled_loss_from_indices(indices_path=non_missing_frame_indices_path):
+    indices = np.load(indices_path)
+    indices_tensor = tf.constant(indices, dtype=tf.int32)
+    def labeled_loss(y_true, y_pred):
+        y_true = tf.gather(y_true, indices_tensor, axis=0)
+        y_pred = tf.gather(y_pred, indices_tensor, axis=0)
+        return tf.keras.losses.MeanSquaredError()(y_true, y_pred)
+    return labeled_loss
