1c825158fbf743d78e1769ef4db821e8509281e9
mingxiaowei <mingxiaowei@berkeley.edu>
2025-02-05 17:24:49 -0800

    create 5 smaller models

--- sleap/jelly/python/construct_model.py
+++ sleap/jelly/python/construct_model.py
@@ -1,3 +1,8 @@
 import sleap
 import numpy as np
 
+def construct_model(raw_model: sleap.Labels, tb_cnt: int=17) -> sleap.Labels:
+    skeleton = sleap.Skeleton(name=f'TB')
+    skeleton.add_node(f'tb')
+    raw_model.skeletons = [skeleton]
+    
