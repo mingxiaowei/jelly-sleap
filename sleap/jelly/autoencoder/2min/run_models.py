dff386e23e4a57f5e13c36aadf1762053371952a
mingxiaowei <mingxiaowei@berkeley.edu>
2025-02-09 14:51:03 -0800

    train on raw coords and validate on corrected ones

--- sleap/jelly/autoencoder/2min/run_models.py
+++ sleap/jelly/autoencoder/2min/run_models.py
@@ -12,12 +12,24 @@ def split_train_val(X, train_size=0.8):
     X_train, X_val = X[:train_size], X[train_size:]
     return X_train, X_val
 
+def load_points(slice_idx=None):
+    coords_corrected = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/manual_5min_c0_points.npy')
+    coords_raw = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/c1_raw_points.npy')
+    if slice_idx is None:
+        return coords_raw, coords_corrected
+    else:
+        return coords_raw, coords_raw[:slice_idx], coords_corrected, coords_corrected[:slice_idx]
+
+def load_data(window_getter, slice_idx=2500, window_size=5):
+    coords_raw, coords_raw_sliced, coords_corrected, coords_corrected_sliced = load_points(slice_idx)
+    X = window_getter(coords_raw, window_size=window_size, flat_len=9000)
+    X_sliced = window_getter(coords_raw_sliced, window_size=window_size, flat_len=slice_idx)
+    y = window_getter(coords_corrected, window_size=window_size, flat_len=9000)
+    y_sliced = window_getter(coords_corrected_sliced, window_size=window_size, flat_len=slice_idx)
+    return X, X_sliced, y, y_sliced
+
 def load_data_1(slice_idx=2500, window_size=5):
-    coords = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/manual_5min_c0_points.npy')
-    coords_sliced = coords[:slice_idx].copy()
-    X = get_sliding_windows_1(coords, window_size=window_size, flat_len=9000)
-    X_sliced = get_sliding_windows_1(coords_sliced, window_size=window_size, flat_len=slice_idx)
-    return X, X_sliced
+    return load_data(get_sliding_windows_1, slice_idx, window_size)
 
 def get_sliding_windows_1(coords, window_size=5, flat_len=2500):
     coords_normalized = coords.astype('float32') / [170, 174]
@@ -38,16 +50,19 @@ def get_sliding_windows_1(coords, window_size=5, flat_len=2500):
 
     return np.array(X)
 
-def run_model_1(train_slice_only=True, window_size=5):
+def run_model_1(train_slice_only=True, window_size=5, epochs=50, batch_size=32):
     
-    X, X_sliced = load_data_1(window_size=window_size)
+    X, X_sliced, y, y_sliced = load_data_1(window_size=window_size)
+    
     if train_slice_only:
         X_train, X_val = split_train_val(X_sliced)
+        y_train, y_val = split_train_val(y_sliced)
     else:
         X_train, X_val = split_train_val(X)
-    
+        y_train, y_val = split_train_val(y)
+        
     model = get_model_1(window_size=window_size)
-    model.fit(X_train, X_train, epochs=50, batch_size=32, validation_data=(X_val, X_val))
+    model.fit(X_train, y_train, epochs=epochs, batch_size=32, validation_data=(X_val, y_val))
 
     denoised_windows = model.predict(X)
     denoised_coords = denoised_windows[:, window_size // 2, :]  # Extract middle frame
@@ -58,11 +73,7 @@ def run_model_1(train_slice_only=True, window_size=5):
     return denoised_coords
 
 def load_data_2(slice_idx=2500, window_size=5):
-    coords = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/manual_5min_c0_points.npy')
-    coords_sliced = coords[:slice_idx].copy()
-    X = get_sliding_windows_2(coords, window_size=window_size)
-    X_sliced = get_sliding_windows_2(coords_sliced, window_size=window_size)
-    return X, X_sliced
+    return load_data(get_sliding_windows_2, slice_idx, window_size)
 
 def get_sliding_windows_2(coords, window_size=5):
     X = []
@@ -89,14 +100,16 @@ def reconstruct_full_sequence(predicted_seqs, num_frames=9000):
 
 def run_model_2(train_slice_only=True, window_size=5):
     
-    X, X_sliced = load_data_2(window_size=window_size)
+    X, X_sliced, y, y_sliced = load_data_2(window_size=window_size)
     if train_slice_only:
         X_train, X_val = split_train_val(X_sliced)
+        y_train, y_val = split_train_val(y_sliced)
     else:
         X_train, X_val = split_train_val(X)
+        y_train, y_val = split_train_val(y)
     
     model = get_model_2(window_size=window_size)
-    model.fit(X_train, X_train, epochs=50, batch_size=32, validation_data=(X_val, X_val))
+    model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_val, y_val))
 
     denoised_windows = model.predict(X)
     denoised_coords = reconstruct_full_sequence(denoised_windows)
@@ -120,32 +133,44 @@ def get_sliding_windows_3(data, window_size=5):
 
 def load_data_3(slice_idx=2500, window_size=5):
     
-    coords = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/manual_5min_c0_points.npy')
+    coords_corrected = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/manual_5min_c0_points.npy')
+    coords_raw = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/c1_raw_points.npy')
     video_path = '/home/mingxiao/Desktop/jellyfish/video/video_1_clips/c1_high_res_5min_track_reencoded_0.mp4'
     video = load_video(video_path)
     
     frames_normalized = np.expand_dims(video, -1)  # Add channel dimension
 
-    coords_normalized = coords.astype('float32') / [170, 174]
-    coords_flat = coords_normalized.reshape((9000, 34))  # Flatten coordinates
-    
-    coords_windows = get_sliding_windows_3(coords_flat, window_size=window_size)
+    coords_raw_normalized = coords_raw.astype('float32') / [170, 174]
+    coords_raw_flat = coords_raw_normalized.reshape((9000, 34))  # Flatten coordinates
+    coords_corrected_normalized = coords_corrected.astype('float32') / [170, 174]
+    coords_corrected_flat = coords_corrected_normalized.reshape((9000, 34))  # Flatten coordinates
+
     frames_windows = get_sliding_windows_3(frames_normalized, window_size=window_size)
+    coords_raw_windows = get_sliding_windows_3(coords_raw_flat, window_size=window_size)
+    coords_corrected_windows = get_sliding_windows_3(coords_corrected_flat, window_size=window_size)
     
-    coords_sliced = coords_flat[:slice_idx]
+    coords_raw_sliced = coords_raw_flat[:slice_idx]
+    coords_corrected_sliced = coords_corrected_flat[:slice_idx]
     frames_sliced = frames_normalized[:slice_idx]
-    coords_windows_sliced = get_sliding_windows_3(coords_sliced, window_size=window_size)
+    
+    coords_raw_windows_sliced = get_sliding_windows_3(coords_raw_sliced, window_size=window_size)
+    coords_corrected_windows_sliced = get_sliding_windows_3(coords_corrected_sliced, window_size=window_size)
     frames_windows_sliced = get_sliding_windows_3(frames_sliced, window_size=window_size)
     
-    return coords_windows, frames_windows, coords_windows_sliced, frames_windows_sliced
+    return coords_raw_windows, coords_corrected_windows, frames_windows, \
+        coords_raw_windows_sliced, coords_corrected_windows_sliced, frames_windows_sliced
 
 def run_model_3(train_slice_only=True, window_size=5):  
-    coords_windows, frames_windows, coords_windows_sliced, frames_windows_sliced = load_data_3(window_size=window_size)
+    coords_raw_windows, coords_corrected_windows, frames_windows, \
+        coords_raw_windows_sliced, coords_corrected_windows_sliced, frames_windows_sliced = load_data_3(window_size=window_size)
+        
     if train_slice_only:
-        coords_train, coords_val = split_train_val(coords_windows_sliced)
+        coords_raw_train, coords_raw_val = split_train_val(coords_raw_windows_sliced)
+        coords_corrected_train, coords_corrected_val = split_train_val(coords_corrected_windows_sliced)
         frames_train, frames_val = split_train_val(frames_windows_sliced)
     else:
-        coords_train, coords_val = split_train_val(coords_windows)
+        coords_raw_train, coords_raw_val = split_train_val(coords_raw_windows)
+        coords_corrected_train, coords_corrected_val = split_train_val(coords_corrected_windows)
         frames_train, frames_val = split_train_val(frames_windows)
     
     model = get_model_3(window_size=window_size)
@@ -154,13 +179,13 @@ def run_model_3(train_slice_only=True, window_size=5):
 
     # Train the model
     model.fit(
-        [frames_train, coords_train], coords_train,
-        validation_data=([frames_val, coords_val], coords_val),
+        [frames_train, coords_raw_train], coords_corrected_train,
+        validation_data=([frames_val, coords_raw_val], coords_corrected_val),
         epochs=50,
         batch_size=32
     )
     
-    denoised_coords = model.predict([frames_windows, coords_windows])
+    denoised_coords = model.predict([frames_windows, coords_raw_windows])
     
     denoised_coords = denoised_coords.reshape((-1, 17, 2))
     denoised_coords = denoised_coords * [170, 174]  # Scale back to original coordinates
@@ -169,12 +194,15 @@ def run_model_3(train_slice_only=True, window_size=5):
     return denoised_coords
 
 def run_model_4(train_slice_only=True, window_size=5):
-    coords_windows, frames_windows, coords_windows_sliced, frames_windows_sliced = load_data_3(window_size=window_size)
+    coords_raw_windows, coords_corrected_windows, frames_windows, \
+        coords_raw_windows_sliced, coords_corrected_windows_sliced, frames_windows_sliced = load_data_3(window_size=window_size)
     if train_slice_only:
-        coords_train, coords_val = split_train_val(coords_windows_sliced)
+        coords_raw_train, coords_raw_val = split_train_val(coords_raw_windows_sliced)
+        coords_corrected_train, coords_corrected_val = split_train_val(coords_corrected_windows_sliced)
         frames_train, frames_val = split_train_val(frames_windows_sliced)
     else:
-        coords_train, coords_val = split_train_val(coords_windows)
+        coords_raw_train, coords_raw_val = split_train_val(coords_raw_windows)
+        coords_corrected_train, coords_corrected_val = split_train_val(coords_corrected_windows)
         frames_train, frames_val = split_train_val(frames_windows)
         
     model = get_model_4(window_size=window_size)
@@ -182,8 +210,8 @@ def run_model_4(train_slice_only=True, window_size=5):
     model.summary()
     
     model.fit(
-        [frames_train, coords_train], coords_train,
-        validation_data=([frames_val, coords_val], coords_val),
+        [frames_train, coords_raw_train], coords_corrected_train,
+        validation_data=([frames_val, coords_raw_val], coords_corrected_val),
         epochs=100,
         batch_size=32,
         callbacks=[
@@ -192,33 +220,45 @@ def run_model_4(train_slice_only=True, window_size=5):
     )
 
     # Generate improved coordinates
-    denoised_windows = model.predict([frames_windows, coords_windows])
+    denoised_windows = model.predict([frames_windows, coords_raw_windows])
     denoised_coords = denoised_windows[:, window_size//2, :].reshape(-1, 17, 2) * [170, 174]
     
     return denoised_coords
 
 def load_data_5(slice_idx=2500, window_size=5):
-    coords = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/manual_5min_c0_points.npy')
-    coords_normalized = (coords / np.array([170, 174])).reshape((9000, 34))
-    coords_windows = get_sliding_windows_3(coords_normalized, window_size=window_size)
+    coords_corrected = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/manual_5min_c0_points.npy')
+    coords_raw = np.load('/home/mingxiao/Desktop/jellyfish/video/video_1_clips/c1_raw_points.npy')
     
-    coords_sliced = coords_normalized[:slice_idx]
-    coords_windows_sliced = get_sliding_windows_3(coords_sliced, window_size=window_size)
+    coords_corrected_normalized = (coords_corrected / np.array([170, 174])).reshape((9000, 34))
+    coords_raw_normalized = (coords_raw / np.array([170, 174])).reshape((9000, 34))
     
-    return coords_windows, coords_windows_sliced
+    coords_corrected_windows = get_sliding_windows_3(coords_corrected_normalized, window_size=window_size)
+    coords_raw_windows = get_sliding_windows_3(coords_raw_normalized, window_size=window_size)
+    
+    coords_corrected_sliced = coords_corrected_normalized[:slice_idx]
+    coords_raw_sliced = coords_raw_normalized[:slice_idx]
+    
+    coords_corrected_windows_sliced = get_sliding_windows_3(coords_corrected_sliced, window_size=window_size)
+    coords_raw_windows_sliced = get_sliding_windows_3(coords_raw_sliced, window_size=window_size)
+    
+    return coords_raw_windows, coords_raw_windows_sliced, \
+        coords_corrected_windows, coords_corrected_windows_sliced
+        
     
 def run_model_5(train_slice_only=True, window_size=5):
     
-    X, X_sliced = load_data_5(window_size=window_size)
+    X, X_sliced, y, y_sliced = load_data_5(window_size=window_size)
     if train_slice_only:
         X_train, X_val = split_train_val(X_sliced)
+        y_train, y_val = split_train_val(y_sliced)
     else:
         X_train, X_val = split_train_val(X)
-    
+        y_train, y_val = split_train_val(y)
+        
     model = get_model_5(window_size=window_size)
     model.compile(optimizer="adam", loss=masked_mse_loss)
     
-    model.fit(X_train, X_train, epochs=100, batch_size=32, validation_data=(X_val, X_val))
+    model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_val, y_val))
     
     denoised = model.predict(X)
     improved_coords = denoised[:, window_size//2, :]  # Extract center frame
@@ -227,20 +267,21 @@ def run_model_5(train_slice_only=True, window_size=5):
     return improved_coords
 
 def run_model_6(train_slice_only=True, window_size=5):
-    X, X_sliced = load_data_5(window_size=window_size)
+    X, X_sliced, y, y_sliced = load_data_5(window_size=window_size)
     if train_slice_only:
         X_train, X_val = split_train_val(X_sliced)
+        y_train, y_val = split_train_val(y_sliced)
     else:
         X_train, X_val = split_train_val(X)
-    
+        y_train, y_val = split_train_val(y)
     model = get_model_6(window_size=window_size)
     model.compile(optimizer="adam", loss=masked_mse_loss)
     
     model.fit(
-        X_train, X_train,
+        X_train, y_train,
         epochs=100,
         batch_size=32,
-        validation_data=(X_val, X_val),
+        validation_data=(X_val, y_val),
         # callbacks=[tf.keras.callbacks.EarlyStopping(patience=10)]
     )
     
@@ -250,7 +291,7 @@ def run_model_6(train_slice_only=True, window_size=5):
     
     return improved_coords
 
-def run_model_7(train_slice_only=True, window_size=5):
+def run_model_7(train_slice_only=True, window_size=5, epochs=100):
     # Force CPU usage
     with tf.device('/CPU:0'):
         coords_windows, frames_windows, coords_windows_sliced, frames_windows_sliced = load_data_3(window_size=window_size)
@@ -267,7 +308,7 @@ def run_model_7(train_slice_only=True, window_size=5):
         model.fit(
             [frames_train, coords_train], 
             coords_train,  # Predict center frame
-            epochs=100,
+            epochs=epochs,
             batch_size=32,
             validation_data=([frames_val, coords_val], coords_val)
         )
