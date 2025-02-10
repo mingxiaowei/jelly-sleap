8f491a6ce5a615ca6f4fa14b2585e42637bcf4a3
mingxiaowei <mingxiaowei@berkeley.edu>
2025-02-10 01:09:27 -0800

    some more attempts

--- sleap/jelly/autoencoder/2min/run_single_model.py
+++ sleap/jelly/autoencoder/2min/run_single_model.py
@@ -1,5 +1,7 @@
 from run_models import *
 
-prediction_18 = run_model_7(train_slice_only=True, window_size=5, epochs=50)
-print(prediction_18.shape)
-np.save('../results/prediction_18.npy', prediction_18)
+with tf.device('/CPU:0'):
+    model_save_path = '../models/model_19.keras'
+    prediction_19 = run_model_7(train_slice_only=True, window_size=5, epochs=20, optimizer=Adam(1e-4), model_save_path=model_save_path)
+    print(prediction_19.shape)
+    np.save('../results/prediction_19.npy', prediction_19)
